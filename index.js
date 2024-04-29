fd61dd94f377f1657b47c644316318df927da33c
thiennt <thiennt@senprints.com>
2024-04-29 14:38:01 +0700

    init project

---
+++ index.js
@@ -0,0 +1,74 @@
+import ping from 'ping'
+import axios from 'axios'
+
+const DOMAINS = ['google.com', 'facebook.com', 'twitter.com']
+const TIMEOUT = 300 // ms
+const INTERVAL = 500 // ms
+const MAX_TRIES = 4
+const DISCORD_WEBHOOK_URL = ''
+const DEBUG = false
+
+async function customPing(domain) {
+    try {
+        let res = await awaitWithTimout(await ping.promise.probe(domain))
+        const { alive, time } = res
+        return { domain, status: alive && time < TIMEOUT }
+    } catch (error) {
+        console.error(`Error pinging ${domain}:`, error)
+        return { domain, status: false }
+    }
+}
+
+async function logErrorToDiscord(errorMessage) {
+    if (!DISCORD_WEBHOOK_URL || DISCORD_WEBHOOK_URL === '') {
+        console.error('No Discord webhook URL provided')
+    }
+
+    try {
+        await axios.post(DISCORD_WEBHOOK_URL, { content: errorMessage })
+        if (DEBUG) {
+            console.log('Error logged to Discord:', errorMessage)
+        }
+    } catch (error) {
+        console.error('Error logging error to Discord:', error)
+    }
+}
+
+async function awaitWithTimout(promise, ms) {
+    return await Promise.race([promise, new Promise((_, reject) => setTimeout(() => reject('Timeout'), ms))])
+}
+
+async function main() {
+    const tracker = DOMAINS.reduce((acc, domain) => {
+        acc[domain] = 0
+        return acc
+    }, {})
+
+    setInterval(async () => {
+        try {
+            const results = await Promise.all(DOMAINS.map(domain => customPing(domain)))
+
+            results.forEach(({ domain, status }) => {
+                if (status) {
+                    if (DEBUG) {
+                        console.log(`Domain ${domain} is up`)
+                    }
+                    tracker[domain] = 0
+                } else {
+                    tracker[domain]++
+                    if (tracker[domain] >= MAX_TRIES) {
+                        if (DEBUG) {
+                            console.log(`Domain ${domain} is down`)
+                        }
+                        logErrorToDiscord(`Domain ${domain} is down`)
+                        tracker[domain] = 0
+                    }
+                }
+            })
+        } catch (error) {
+            console.error('Error during ping operations:', error)
+        }
+    }, INTERVAL)
+}
+
+await main()
---
+++ readme.md
@@ -0,0 +1,7 @@
+```shell
+# 1. Install dependencies
+npm install
+
+# 2. Run the server
+node index.js
+```
